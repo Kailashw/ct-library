7986bddf33a204b7b9f81c083752fa745d9f2898
Kailash CT <kailas@cloudthing.com>
2019-05-15 00:06:01 +0530

    fbconfigs changes

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -3,23 +3,14 @@ import 'firebase/firestore'
 import 'firebase/auth'
 
 // Initialize Firebase.
-var config = {
-    apiKey: "AIzaSyBu70K3ut-E8MZVFK9uDJY0b9ZcVSzXrps",
-    authDomain: "ct-library-fae50.firebaseapp.com",
-    databaseURL: "https://ct-library-fae50.firebaseio.com",
-    projectId: "ct-library-fae50",
-    storageBucket: "ct-library-fae50.appspot.com",
-    messagingSenderId: "95561275819"
+  var config = {
+    apiKey: null,
+    authDomain: null,
+    databaseURL: null,
+    projectId: null,
+    storageBucket: null,
+    messagingSenderId: null
   };
-
-  // var config = {
-  //   apiKey: null,
-  //   authDomain: null,
-  //   databaseURL: null,
-  //   projectId: null,
-  //   storageBucket: null,
-  //   messagingSenderId: null
-  // };
 
 firebase.initializeApp(config);
 firebase.firestore().settings({ timestampsInSnapshots : true})
